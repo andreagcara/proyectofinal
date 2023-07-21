88ade2a0b7e48ec3d3c17db7bb39411f4166f0bd
andreagcara <116802411+andreagcara@users.noreply.github.com>
2023-07-22 00:08:24 +0200

    base de datos

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async() =>{
     try{
-        await mongoose.connect(process.env.DATABASE_URL ||"mongodb://0.0.0.0:27017/proyectodb");
+        await mongoose.connect(process.env.MONGO_URL ||"mongodb://0.0.0.0:27017/proyectodb");
         console.log(">>> DB is conected");
     } catch (error) {
         console.log(error);
